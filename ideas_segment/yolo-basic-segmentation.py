4f64819eb3f8b49da78c92e1c33e2f37a990e0da
Rakrocks18 <rakshitshah015@gmail.com>
2024-09-04 10:55:28 +0530

    trial image and removed unaccessed modules

--- ideas_segment/yolo-basic-segmentation.py
+++ ideas_segment/yolo-basic-segmentation.py
@@ -1,10 +1,7 @@
 from ultralytics import YOLO
-import random
-import cv2
-import numpy as np
 
 model = YOLO("yolov8n-seg.pt")
 
-results = model("/content/meowwwwwwww.jpeg")
+results = model("../trials_images/meowwwwwwww.jpeg")
 for result in results:
     result.save(filename="seg_img.jpg")
